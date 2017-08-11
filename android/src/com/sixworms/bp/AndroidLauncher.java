0d05adfe8ccb6b3e49b6aa1855e3e667f2d99c8f
Anton Chudinov <sixworms@gmail.com>
2017-08-11 18:36:33 +0500

    project added

---
+++ android/src/com/sixworms/bp/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sixworms.bp;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sixworms.bp.BP;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BP(), config);
+	}
+}
